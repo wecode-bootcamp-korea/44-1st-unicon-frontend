4a0e0dc3fb260afebbc712a9410ea9fa600d5f83
George Kim <ggkim0614@gmail.com>
2023-04-03 17:30:18 +0900

    Fix : initial commit 라우팅 관련 일부 수정

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 function Login() {
-  return (
-    <div>Login.js
-    </div>
-  );
+  return <div>Login.js</div>;
 }
 
 export default Login;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 function Main() {
-  return (
-    <div>Main.js
-    </div>
-  );
+  return <div>Main.js</div>;
 }
 
 export default Main;
