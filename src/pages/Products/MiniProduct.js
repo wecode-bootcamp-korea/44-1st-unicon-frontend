681b4d9074331f149d2b09d8eee176c96919d34b
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-04-04 14:01:31 +0900

    Add: Products 레이아웃

---
+++ src/pages/Products/MiniProduct.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Heart, ShoppingCart } from 'react-feather';
+import './MiniProduct.scss';
+
+const MiniProduct = ({ id, img, name, commit, price }) => {
+  return (
+    <div className="mini-product" key={id}>
+      <div className="img">{img}</div>
+      <div className="text-box">
+        <p className="title">{name}</p>
+        <p className="commit">{commit}</p>
+        <span className="price-box">
+          <p>￦</p>
+          <p>{price}</p>
+        </span>
+        <div className="star-wrap">
+          <span className="star">★★★★☆ (16)</span>
+        </div>
+
+        <div className="icon-box">
+          <div className="cart-wrap">
+            <button className="cart-btn">
+              <ShoppingCart className="cart-icon" />
+            </button>
+          </div>
+          <Heart className="heart" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MiniProduct;
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,8 +1,121 @@
 import React from 'react';
+import MiniProduct from './MiniProduct';
+import { ChevronDown } from 'react-feather';
 import './Products.scss';
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div className="product">
+      <div className="inner-menu-box">
+        {INNERMENU.map(info => {
+          return (
+            <div className="inner-menu-item" key={info.id}>
+              <div className="img" />
+              <p className="text">{info.name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className="filter-menu">
+        <button>
+          정렬
+          <ChevronDown className="filter-icon" />
+        </button>
+        <button>
+          베스트샐러
+          <ChevronDown className="filter-icon" />
+        </button>
+        <button>
+          가격
+          <ChevronDown className="filter-icon" />
+        </button>
+        <button>
+          색상
+          <ChevronDown className="filter-icon" />
+        </button>
+      </div>
+      <div className="product-box">
+        {PRICE.map(data => (
+          <MiniProduct
+            key={data.id}
+            name={data.name}
+            commit={data.commit}
+            price={data.price}
+            img={data.img}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Products;
+
+const INNERMENU = [
+  { id: 1, name: '일반 조명' },
+  { id: 2, name: '장식 조명' },
+  { id: 3, name: '시스템 조명' },
+  { id: 4, name: '스마트 조명' },
+  { id: 5, name: '야외용 조명' },
+  { id: 6, name: '욕실 조명' },
+  { id: 7, name: 'LED 전구' },
+];
+
+const PRICE = [
+  {
+    id: 8,
+    name: '일반 조명',
+    img: 'https://www.ikea.com/kr/ko/images/products/pello-armchair-holmby-natural__38296_pe130209_s5.jpg?f=xs',
+    commit: '멋진 조명',
+    price: 10000,
+  },
+  {
+    id: 9,
+    name: '장식 조명',
+    img: 'https://www.ikea.com/kr/ko/images/products/pello-armchair-holmby-natural__38296_pe130209_s5.jpg?f=xs',
+    commit: '따듯한 조명',
+    price: 42000,
+  },
+  {
+    id: 10,
+    name: '시스템 조명',
+    img: 'https://www.ikea.com/kr/ko/images/products/pello-armchair-holmby-natural__38296_pe130209_s5.jpg?f=xs',
+    commit: '밝다',
+    price: 30000,
+  },
+  {
+    id: 11,
+    name: '스마트 조명',
+    img: 'https://www.ikea.com/kr/ko/images/products/pello-armchair-holmby-natural__38296_pe130209_s5.jpg?f=xs',
+    commit: '이쁘다',
+    price: 29000,
+  },
+  {
+    id: 12,
+    name: '야외용 조명',
+    img: 'https://www.ikea.com/kr/ko/images/products/pello-armchair-holmby-natural__38296_pe130209_s5.jpg?f=xs',
+    commit: '멋지다',
+    price: 17000,
+  },
+  {
+    id: 13,
+    name: '욕실 조명',
+    img: 'https://www.ikea.com/kr/ko/images/products/pello-armchair-holmby-natural__38296_pe130209_s5.jpg?f=xs',
+    commit: '굉장해',
+    price: 72000,
+  },
+  {
+    id: 14,
+    name: 'LED 전구(백색)',
+    img: 'https://www.ikea.com/kr/ko/images/products/pello-armchair-holmby-natural__38296_pe130209_s5.jpg?f=xs',
+    commit: '사라',
+    price: 13000,
+  },
+  {
+    id: 15,
+    name: 'LED 전구(주광색)',
+    img: 'https://www.ikea.com/kr/ko/images/products/pello-armchair-holmby-natural__38296_pe130209_s5.jpg?f=xs',
+    commit: '사라고',
+    price: 18000,
+  },
+];
