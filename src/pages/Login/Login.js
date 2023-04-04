ab2e88e2b02d077d3ec2030047242524dc5f25b2
George Kim <ggkim0614@gmail.com>
2023-04-04 16:02:03 +0900

    Add : Login 컴포넌트 레이아웃, 유효성 검사 기능 추가

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -11,7 +11,10 @@ function Login() {
 
   const { userId, password } = inputs;
 
+  const navigate = useNavigate();
+
   const [passwordVisible, setPasswordVisible] = useState(false);
+
   const [warning, setWarning] = useState({
     idIncorrect: false,
     pwIncorrect: false,
@@ -34,11 +37,11 @@ function Login() {
   };
 
   const handleSubmit = () => {
-    console.log(inputs);
+    allInputValid ? console.log(inputs) : alert('정보값을 확인해주세요!');
   };
 
   const userIdValid = userId.match(/^\S+@\S+\.\S+$/);
-  const passwordValid = password.length > 7;
+  const passwordValid = password.match(/^(?=.*[A-Za-z])(?=.*[0-9])(?=.{8,20})/);
 
   const allInputValid = userIdValid && passwordValid;
 
@@ -115,7 +118,7 @@ function Login() {
           </div>
           {pwIncorrect && (
             <div className="warning">
-              <AlertCircle className="icon" /> 비밀번호를 8자 이상 입력해주세요.
+              <AlertCircle className="icon" /> 올바른 비밀번호를 입력해주세요.
             </div>
           )}
           <div className="checkbox-container">
@@ -123,6 +126,7 @@ function Login() {
             <span className="label">로그인 상태 유지</span>
           </div>
           <button
+            onClick={handleSubmit}
             className={allInputValid ? 'submit-btn' : 'submit-btn disabled'}
           >
             로그인
@@ -131,7 +135,7 @@ function Login() {
             <div className="text">
               IKEA 계정이 없으신가요? 지금 바로 만들어보세요.
             </div>
-            <button onClick={handleSubmit} className="btn">
+            <button onClick={() => navigate('/signup')} className="btn">
               회원 가입하기
             </button>
           </div>
