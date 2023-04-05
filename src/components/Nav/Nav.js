23d89bb9b6cd719ca9d2694f66fd58907b23ddaf
George Kim <ggkim0614@gmail.com>
2023-04-05 21:53:34 +0900

    Add : Nav 컴포넌트 레이아웃 추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Truck, ShoppingBag, Phone, Menu } from 'react-feather';
+import { Link } from 'react-router-dom';
+import {
+  Truck,
+  Phone,
+  Menu,
+  User,
+  Heart,
+  ShoppingBag,
+  Home,
+} from 'react-feather';
 import './Nav.scss';
 
 const Nav = () => {
@@ -21,18 +30,83 @@ const Nav = () => {
           </div>
         </div>
       </div>
-      <div className="grid-wrapper">
+      <div className="center-wrapper">
         <div className="side-nav-container">
-          <Menu />
+          <div className="menu-icon">
+            <Menu />
+          </div>
           <p>메뉴</p>
         </div>
         <div className="nav-center-section">
-          <div className="wekea-logo" />
+          <div className="logo-input-wrapper">
+            <div className="wekea-logo" />
+
+            <input className="input" type="text" placeholder="검색어 입력" />
+          </div>
+          <span className="btn-container">
+            <div className="icon-text-btn">
+              <User width={20} height={20} />
+              <p>Hej! 로그인 또는 가입하기</p>
+            </div>
+            <div className="icon-text-btn">
+              <Heart width={20} height={20} />
+            </div>
+            <div className="icon-text-btn">
+              <ShoppingBag width={20} height={20} />
+            </div>
+          </span>
         </div>
         <div />
+      </div>
+      <div className="second-row">
+        <div>
+          {NAV_FILTER_LINKS.map(link => (
+            <Link key={link.id} path={link.path} className="nav-filter-link">
+              {link.name}
+            </Link>
+          ))}
+        </div>
+        <span className="btn-container">
+          <div className="icon-text-btn">
+            <Truck width={20} height={20} />
+            <p>우편 번호 입력</p>
+          </div>
+          <div className="icon-text-btn">
+            <Home width={20} height={20} />
+            <p>매장 선택</p>
+          </div>
+        </span>
       </div>
     </div>
   );
 };
 
 export default Nav;
+
+const NAV_FILTER_LINKS = [
+  {
+    id: 1,
+    name: '모든 제품',
+    path: '/',
+  },
+  {
+    id: 2,
+    name: '온라인 쇼룸',
+    path: '/',
+  },
+  {
+    id: 3,
+    name: '특별한 가격',
+    path: '/',
+  },
+  {
+    id: 4,
+    name: '홈 액세서리',
+    path: '/',
+  },
+  {
+    id: 5,
+    name: '서비스',
+    path: '/',
+  },
+];
