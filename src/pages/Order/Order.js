5a8b7d0f6203a6fc3694036dbbe4f8b43890a68a
George Kim <ggkim0614@gmail.com>
2023-04-08 20:53:43 +0900

    wip

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -5,8 +5,15 @@ import './Order.scss';
 
 const Order = () => {
   const [orderData, setOrderData] = useState([]);
-
+  const [modalOpen, setModalOpen] = useState(false);
   const navigate = useNavigate();
+
+  const [requirements, setRequirements] = useState({
+    delivery: false,
+    terms: false,
+  });
+
+  const { delivery, terms } = requirements;
 
   useEffect(() => {
     fetch('http://localhost:3000/data/cartData.json', {
@@ -22,19 +29,32 @@ const Order = () => {
       });
   }, []);
 
+  console.log(requirements);
   console.log(orderData);
 
   return (
     <div className="order">
+      {modalOpen && (
+        <>
+          <div onClick={() => setModalOpen(!modalOpen)} className="modal" />
+          <div className="modal-box">
+            <div className="header">결제</div>
+            <div className="total-wrapper">
+              <div className="label">총 결제 금액:</div>
+              <div className="cost">₩ 79,000</div>
+            </div>
+            <div className="option-wrapper">
+              <input className="radio" type="radio" />
+              <div className="option">이케아 포인트</div>
+            </div>
+          </div>
+        </>
+      )}
+
       <div className="section-top">
         <div className="logo" />
       </div>
       <div className="section-main">
-        <div className="column-num">
-          <div className="step-num-bg">
-            <div className="step-num">1</div>
-          </div>
-        </div>
         <div className="section-text">
           <div className="section-header">
             <div className="step-header">배송과 픽업 방법</div>
@@ -45,7 +65,7 @@ const Order = () => {
             수정
           </div>
           <div className="delivery-type-header">배송 방법</div>
-          <div className="delivery-option-box">
+          <div className={`delivery-option-box ${delivery && `active`}`}>
             <div className="title-wrap">
               <div className="delivery-option-title">무료 배송</div>
               <div className="delivery-option-title">₩ 0</div>
@@ -62,12 +82,57 @@ const Order = () => {
             <div className="no-contact-box">
               <div className="delivery-option-title">비대면 배송</div>
               <div className="checkbox-container">
-                <input type="checkbox" className="checkbox" />
+                <input
+                  onClick={() =>
+                    setRequirements({ ...requirements, delivery: !delivery })
+                  }
+                  type="checkbox"
+                  className="checkbox"
+                />
                 <div className="checkbox-label">제품을 현관 앞에 놔두기</div>
               </div>
             </div>
           </div>
-          <button className="btn-cta">계속</button>
+          <div className={!delivery && 'inactive'}>
+            <div className="section-header">
+              <div className="step-header">결제</div>
+            </div>
+            <div className="payment-options">
+              <div className="payment-text">위케아 포인트</div>
+              <div className="option-thumbs-1" />
+              <div className="option-thumbs-2" />
+            </div>
+            <div className="cost">
+              <div className="total">총 주문금액</div>
+              <div className="cost-num">₩ 79,000</div>
+            </div>
+            <div className="terms-wrapper">
+              <input
+                onClick={() =>
+                  setRequirements({ ...requirements, terms: !terms })
+                }
+                type="checkbox"
+                className="terms"
+              />
+              <div>이용 약관을 읽었으며 동의합니다.</div>
+            </div>
+            <div
+              onClick={() => setModalOpen(!modalOpen)}
+              className={`btn-purchase ${(!delivery || !terms) && `disabled`}`}
+            >
+              <div className="btn-content">
+                <div className="btn-text">결제하기</div>
+                <div className="arrow-wrapper">
+                  <Icons
+                    name="ArrowRight"
+                    className="arrow"
+                    width={24}
+                    height={24}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="right">
           <div className="content-right">
@@ -92,8 +157,8 @@ const Order = () => {
               <div className="title">주문 내역</div>
               <div className="header">
                 <div className="sum-text">
-                  <div>제품 가격(배송비 재외)</div>
-                  <div>₩ 0</div>
+                  <div>제품 가격 (배송비 제외)</div>
+                  <div>₩ 79,000</div>
                 </div>
                 <div className="sum-text">
                   <div>배송비</div>
