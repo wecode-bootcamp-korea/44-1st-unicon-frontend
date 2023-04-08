7590d21327ac2c3212064e8b1622d8451c307690
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-04-08 22:36:33 +0900

    Add: 상수데이터 수정

---
+++ src/pages/Main/COLOR_BOX.js
@@ -0,0 +1,37 @@
+export const COLOR_BOX = [
+  {
+    id: 1,
+    name: '침실',
+    img: 'https://www.ikea.com/ext/ingkadam/m/3aa319771247890b/original/PH171699-crop001.jpg?f=xxs',
+  },
+  {
+    id: 2,
+    name: '수납/정리',
+    img: 'https://www.ikea.com/ext/ingkadam/m/57af553408fd829f/original/PE867858-crop001.jpg?f=xxs',
+  },
+  {
+    id: 3,
+    name: '홈오피스',
+    img: 'https://www.ikea.com/images/f0/87/f087331e4416c4ce01d265d6c4288e9b.jpg?f=xxs',
+  },
+  {
+    id: 4,
+    name: '다이닝',
+    img: 'https://www.ikea.com/ext/ingkadam/m/206171a86cff819c/original/PH166826-crop001.jpg?f=xxs',
+  },
+  {
+    id: 5,
+    name: '거실',
+    img: 'https://www.ikea.com/ext/ingkadam/m/7a90f1e0176f5447/original/PH167387-crop001-crop002.jpg?f=xxs',
+  },
+  {
+    id: 6,
+    name: '주방',
+    img: 'https://www.ikea.com/ext/ingkadam/m/7e56e5a9f682485f/original/PH181607.jpg?f=xxs',
+  },
+  {
+    id: 7,
+    name: '홈데코/장식품',
+    img: 'https://www.ikea.com/ext/ingkadam/m/2cefdac0dd90b2a3/original/PH159748-crop002.jpg?f=xxs',
+  },
+];
