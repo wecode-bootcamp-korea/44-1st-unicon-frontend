0623a63d02423721afea7334e90eddb6971afca9
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-04-11 10:56:20 +0900

    Add: 댓글컴포넌트 분류
    git commit --amend

--- src/pages/ProductDetail/components/ReviewBox/ReviewBox.js
+++ src/pages/ProductDetail/components/ReviewBox/ReviewBox.js
@@ -3,7 +3,13 @@ import React from 'react';
 const ReviewBox = () => {
   return (
     <div className="review-box">
-      <div>댓글기능</div>
+      <div className="title-box">
+        <p>상품평을 작성해주세요.</p>
+        <p>
+          최근 구매한 제품이 마음에 드시나요? 다른 사람들과 구매 경험을
+          공유해보세요!
+        </p>
+      </div>
     </div>
   );
 };
---
+++ src/pages/ProductDetail/components/ReviewBox/components/ReviewInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ReviewInput.scss';
+
+const ReviewInput = () => {
+  return (
+    <div className="review-input">
+      <input className="input" />
+      <button className="btn" />
+    </div>
+  );
+};
+
+export default ReviewInput;
