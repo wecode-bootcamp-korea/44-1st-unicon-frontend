046168d108fd970e5439c5e839c41f7215658c66
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-04-11 17:42:47 +0900

    wip

--- src/pages/ProductDetail/components/ReviewBox/ReviewInput.js
+++ src/pages/ProductDetail/components/ReviewBox/ReviewInput.js
@@ -2,10 +2,23 @@ import React from 'react';
 import './ReviewInput.scss';
 
 const ReviewInput = () => {
+  const uploadReview = () => {};
   return (
     <div className="review-input">
-      <input className="input" />
-      <button className="btn">상품 리뷰 올리기</button>
+      <input
+        onChange={e => e.target.value}
+        className="input"
+        type="text"
+        placeholder="WEKEA 상품은 만족스러우셨나요?"
+      />
+      <button
+        className="btn"
+        onClick={() => {
+          uploadReview();
+        }}
+      >
+        상품 리뷰 올리기
+      </button>
     </div>
   );
 };
